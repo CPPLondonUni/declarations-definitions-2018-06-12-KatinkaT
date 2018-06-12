f0593b8cdfa0f3f094de353abbb7cf5eb28b2cc4
Kalvarez <kalvarez2390@gmail.com>
2018-06-12 19:50:11 +0100

    Add files via upload

---
+++ DefAndDec/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+
+#include "utility.h"
+
+
+
+//Leave main() alone! THAT MEANS DON'T TOUCH CODE BELOW THIS LINE!!!
+int main() {
+	std::cout << "Enter a name: ";
+	auto name = ReadStringFromConsole();
+	std::cout << "Enter an adjective: ";
+	auto adjt = ReadStringFromConsole();
+
+	std::cout << DescribePersonAsSomething(name, adjt) << '\n';
+	return 0;
+}
+
---
+++ DefAndDec/utility.h
@@ -0,0 +1,13 @@
+
+#ifndef DEFANDDEC_HEAD_H
+#define DEFANDDEC_HEAD_H
+
+#include <string>
+
+std::string ReadStringFromConsole();
+std::string ret;
+
+std::string DescribePersonAsSomething(const std::string&, const std::string&);
+
+
+#endif
